7bbd847d1d5f2e2c2244911f40d52de78c87f565
buloko <buloko8@gmail.com>
2022-12-13 15:56:11 -0500

    created repo,gitignore, added json and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require(`inquirer`);
+const fs = require('fs');
+
+const { writeFile } = require('fs/promises');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        
+    ])
+}
